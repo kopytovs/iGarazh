6d20f67f2cee751ccc41e5aafcf4d633a1eacd80
kopytovs <kopytov@me.com>
2016-12-14 10:45:05 +0300

    1.0(3)
    + design!!!

--- iGarazh/AppDelegate.swift
+++ iGarazh/AppDelegate.swift
@@ -18,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UITabBar.appearance().tintColor = UIColor.purple
+        
+        UINavigationBar.appearance().tintColor = UIColor.purple
+        
         return true
     }
     
